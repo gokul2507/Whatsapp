2ae064bec92df2fbb805fe7f53c802351c940caa
Gokul R <srigokulrajendiran@gmail.com>
2021-10-28 22:09:09 +0530

    -

---
+++ src/Components/Content.jsx
@@ -0,0 +1,31 @@
+import React, { Component } from 'react'
+
+export default class Content extends Component {
+     constructor(props){
+          super(props);
+          this.state={
+               value:"",
+          }
+     }
+     change=e=>{
+          this.setState({value:e.target.value})
+     }
+     render() {
+          return (
+               <div>
+                    <img src="whatsapp.ico" alt="" />
+                    
+                    <br /><input className="number" type="number" name="number" id="number" onChange={this.change} />
+                    <br />
+                    <a
+                      className="App-link"
+                      href={"https://web.whatsapp.com/send?phone=+91"+this.state.value+"&text=Hi"}
+                      target="_blank"
+                      rel="noreferrer" 
+                    >
+                      {this.state.value}
+                    </a>
+               </div>
+          )
+     }
+}
